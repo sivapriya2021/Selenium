7eb6a239fe2e39c12ea543c4da3ba7ebbb655df5
mohitha <mohitha@DESKTOP-NDVQ92D.attlocal.net>
2021-02-02 23:44:42 -0600

    SecondCommit

--- com.Selenium.AppTests/src/test/java/com/Selenium/AppScripts/AppTestScripts.java
+++ com.Selenium.AppTests/src/test/java/com/Selenium/AppScripts/AppTestScripts.java
@@ -2,6 +2,8 @@ package com.Selenium.AppScripts;
 
 import static org.junit.Assert.assertTrue;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 
@@ -9,10 +11,34 @@ import org.testng.annotations.Test;
 
 public class AppTestScripts 
 {
-   
+   public static WebDriver driver;
+	public static String expurl = "https://www.mycontactform.com/";
+	
+	
+	
     @Test
-    public void launchbrowser()
+    public void launchbrowser() throws InterruptedException
+    
+    
     {
-       System.out.println("Selenium Test");
+       
+    	
+    	
+    	System.setProperty("webdriver.chrome.driver","./mydrivers/chromedriver.exe");
+    	driver = new ChromeDriver();
+    	
+    	driver.get(expurl);
+    	Thread.sleep(3000);
+    	
+    	if(expurl.equals(driver.getCurrentUrl())) {
+    	
+    		System.out.println("Verified URL");
+    	}
+    	else
+    	{
+    	System.out.println("Does not verified");
+    }
+    	
+    	driver.close();
     }
 }
