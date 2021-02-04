de83ca71be6bb9433488c0ac90f7ad600919e65d
mohitha <mohitha@DESKTOP-NDVQ92D.attlocal.net>
2021-02-04 16:38:59 -0600

    FinalCommit

--- com.Selenium.AppTests/src/test/java/com/Selenium/AppUtilities/MavenSampleTest.java
+++ com.Selenium.AppTests/src/test/java/com/Selenium/AppUtilities/MavenSampleTest.java
@@ -1,10 +1,38 @@
 package com.Selenium.AppUtilities;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class MavenSampleTest {
-  @Test
-  public void f() {
+  
+	public static WebDriver driver;
+	public static String expurl = "https://www.mycontactform.com/";
+	
+	
+	
+	@Test
+  public void launchbrowser() throws Throwable {
+	  
+	  
+	  System.setProperty("webdriver.chrome.driver","./mydrivers/chromedriver.exe");
+  	
+  	driver = new ChromeDriver();
+  	
+  	driver.get(expurl);
+  	Thread.sleep(3000);
+  	
+  	if(expurl.equals(driver.getCurrentUrl())) {
+  	
+  		System.out.println("Verified URL");
+  	}
+  	else
+  	{
+  	System.out.println("Does not verified");
+  }
+  	
+  	driver.close();
+	  
 	  
 	  
 	  System.out.println("Java Selenium Test");
